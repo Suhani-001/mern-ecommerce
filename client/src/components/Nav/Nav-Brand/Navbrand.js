541e3f38f4f9f55acd7fa9444b89e3cec70e8c80
mahijain550 <mahii223823@gmail.com>
2026-09-21 00:06:42 +0530

    logo

--- client/src/components/Nav/Nav-Brand/Navbrand.js
+++ client/src/components/Nav/Nav-Brand/Navbrand.js
@@ -1,14 +1,15 @@
-import './NavBrand.css'
+import React from 'react';
 import { Link } from 'react-router-dom';
+import './NavBrand.css';
 
 const NavBrand = () => {
-    return ( 
-        <div href="#home" className='navbrand__container'>
-           <h1 className='navbrand'>
-               <Link to="/">vastra</Link>
+    return (
+        <div className="navbrand__container">
+            <h1 className="navbrand">
+                <Link to="/">vastra</Link>
             </h1>
         </div>
-     );
-}
- 
+    );
+};
+
 export default NavBrand;
